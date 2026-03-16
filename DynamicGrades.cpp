2fd823038da330d8e6bfe3b2a8df37a3f67da4f4
Naomi Quinones <52065567+naomiquinones@users.noreply.github.com>
2026-03-16 09:02:57 +0000

    Add other functions

--- DynamicGrades.cpp
+++ DynamicGrades.cpp
@@ -1,12 +1,47 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 void getScores(double *arr, int size) {
+    if (size <= 0) {
+        cout << "Invalid size for scores array." << endl;
+        return;
+    }
     for (int i = 0; i < size; i++) {
         cout << "Enter score " << (i + 1) << ":" << endl;
         cin >> arr[i];
     }
+}
+void sortScores(double *arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                double temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+void printScores(double *arr, int size) {
+    cout << "Printing Scores:" << endl;
+    cout << fixed << setprecision(2);
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+double calculateAverage(const double* arr, int size) {
+    if (size <= 0) {
+        cout << "Invalid size for scores array." << endl;
+        return 0.0;
+    }
+    double sum = 0.0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum / size;
 }
 int main() {
     int numTests = 0;
@@ -16,6 +51,12 @@ int main() {
     if (numTests > 0) {
         double *scores = new double[numTests];
         getScores(scores, numTests);
+        printScores(scores, numTests);
+        sortScores(scores, numTests);
+        printScores(scores, numTests);
+        double average = calculateAverage(scores, numTests);
+        cout << "Average Score: " << fixed << setprecision(2) << average << endl;
+        delete[] scores;
     }
 
 
