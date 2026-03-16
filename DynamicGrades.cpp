c00b68a7e8637606bff2d005343494f28d28b78d
Naomi Quinones <52065567+naomiquinones@users.noreply.github.com>
2026-03-16 08:48:25 +0000

    Move to getScores

--- DynamicGrades.cpp
+++ DynamicGrades.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+void getScores(double *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << "Enter score " << (i + 1) << ":" << endl;
+        cin >> arr[i];
+    }
+}
 int main() {
     int numTests = 0;
     cout << "How many test scores to process:" << endl;
@@ -9,10 +15,7 @@ int main() {
 
     if (numTests > 0) {
         double *scores = new double[numTests];
-        for (int i = 0; i < numTests; i++) {
-            cout << "Enter score " << (i + 1) << ":" << endl;
-            cin >> scores[i];
-        }
+        getScores(scores, numTests);
     }
 
 
